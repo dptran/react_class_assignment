fa51379db7adc251fb9760cc1a4ee747fb48c446
dptran <darren-tran@hotmail.com>
2021-12-08 09:24:36 -0800

    latest

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react"
 
-// function App() {
-//   const clickButton = () => {
-//     alert("Great Shot!");
-//   }
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Contact</h1>
-//         <p><button onClick={clickButton}>Take the shot!</button></p>
-//         <p><div id="counter">Counter: 0</div></p>
-//       </header>
-//     </div>
-//   );
-// }
-
 function App() {
   const [count, setCount] = useState(0);
   return (
